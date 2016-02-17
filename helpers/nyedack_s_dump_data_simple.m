671ea1b99be3f1a7953bd1f5a611671a55f8dafb
Jeffrey Markowitz <jmarkow@cns.bu.edu>
2016-02-17 17:50:45 -0500

    Bugfix

--- helpers/nyedack_s_dump_data_simple.m
+++ helpers/nyedack_s_dump_data_simple.m
@@ -1,5 +1,5 @@
 function nyedack_s_dump_data_simple(obj,event,fid)
 % write to binary and get out
 
-data = [event.TimeStamps, eventt.Data]' ;
+data = [event.TimeStamps, event.Data]' ;
 fwrite(fid,data,'double');
